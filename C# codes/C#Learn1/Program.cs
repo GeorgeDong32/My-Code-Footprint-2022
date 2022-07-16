45e605795d212f09ee27d85bfd9e456356f29a7c
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-16 22:04:42 +0800

    Update Codes

--- C# codes/C#Learn1/Program.cs
+++ C# codes/C#Learn1/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Signed integral types:");
+﻿/*
+Console.WriteLine("Signed integral types:");
 
 Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
 Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
@@ -15,6 +16,9 @@ Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
 
 Console.WriteLine("");
 Console.WriteLine("Floating point types:");
+
 Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
 Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
 Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
+*/
+int[] data = new int[3];
