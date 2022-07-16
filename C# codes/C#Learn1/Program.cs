85429cbe778357f64cb7856bce2b8f1811fd1d87
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-16 21:21:23 +0800

    Update codes

---
+++ C# codes/C#Learn1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Signed integral types:");
+
+Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
+Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
+Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
+Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
