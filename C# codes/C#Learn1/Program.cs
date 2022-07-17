d082789f927b03ca5872c272f0039ce91a67cb72
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-17 14:36:24 +0800

    First Challenge

--- C# codes/C#Learn1/Program.cs
+++ C# codes/C#Learn1/Program.cs
@@ -1,48 +1,17 @@
-﻿/*
-Console.WriteLine("Signed integral types:");
+﻿string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+string charre = "";
+decimal sum = 0; decimal mid = 0;
+foreach (var val in values)
+{
+    if (decimal.TryParse(val, out mid))
+    {
+        sum += mid;
+    }
+    else
+    {
+        charre += val;
+    }
+}
 
-Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
-Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
-Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
-Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
-
-Console.WriteLine("");
-Console.WriteLine("Unsigned integral types:");
-
-Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
-Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
-Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
-Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
-
-Console.WriteLine("");
-Console.WriteLine("Floating point types:");
-
-Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
-Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
-Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
-
-int[] data = new int[3];
-
-string shortenedString = "Hello World!";
-Console.WriteLine(shortenedString);
-
-int first = 2;
-string second = "4";
-int result = first + second;
-Console.WriteLine(result);
-
-int first = 2;
-string second = "4";
-string result = first + second;
-Console.WriteLine(result);
-
-int myInt = 3;
-Console.WriteLine($"int: {myInt}");
-
-decimal myDecimal = myInt;
-Console.WriteLine($"decimal: {myDecimal}");*/
-
-int first = 5;
-int second = 7;
-string message = first.ToString() + second.ToString();
-Console.WriteLine(message);
+Console.WriteLine($"Message: {charre}");
+Console.WriteLine($"Total: {sum}");
