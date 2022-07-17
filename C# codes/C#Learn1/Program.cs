78715bb313b59a18b3f69fda9a74f8036c1783f8
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-17 10:41:48 +0800

    Update Codes

--- C# codes/C#Learn1/Program.cs
+++ C# codes/C#Learn1/Program.cs
@@ -20,8 +20,13 @@ Console.WriteLine("Floating point types:");
 Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
 Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
 Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
-*/
+
 int[] data = new int[3];
 
 string shortenedString = "Hello World!";
-Console.WriteLine(shortenedString);
+Console.WriteLine(shortenedString);*/
+
+int first = 2;
+string second = "4";
+int result = first + second;
+Console.WriteLine(result);
