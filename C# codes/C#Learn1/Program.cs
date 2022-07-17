f0bb4987a2f081c1b1a61110ece09be228df964f
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-17 11:12:46 +0800

    Update Codes

--- C# codes/C#Learn1/Program.cs
+++ C# codes/C#Learn1/Program.cs
@@ -24,9 +24,26 @@ Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-2
 int[] data = new int[3];
 
 string shortenedString = "Hello World!";
-Console.WriteLine(shortenedString);*/
+Console.WriteLine(shortenedString);
 
 int first = 2;
 string second = "4";
 int result = first + second;
 Console.WriteLine(result);
+
+int first = 2;
+string second = "4";
+string result = first + second;
+Console.WriteLine(result);
+
+int myInt = 3;
+Console.WriteLine($"int: {myInt}");
+
+decimal myDecimal = myInt;
+Console.WriteLine($"decimal: {myDecimal}");*/
+
+decimal myDecimal = 3.14m;
+Console.WriteLine($"decimal: {myDecimal}");
+
+int myInt = (int)myDecimal;
+Console.WriteLine($"int: {myInt}");
