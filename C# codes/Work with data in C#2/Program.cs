1c8e645e8c63631aca7ffc99d418f179bf166142
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-17 23:15:42 +0800

    S4

--- C# codes/Work with data in C#2/Program.cs
+++ C# codes/Work with data in C#2/Program.cs
@@ -52,5 +52,6 @@ foreach (var pallet in pallets)
 string value = "abc123";
 char[] valueArray = value.ToCharArray();
 Array.Reverse(valueArray);
-string result = new string(valueArray);
+// string result = new string(valueArray);
+string result = String.Join(",", valueArray);
 Console.WriteLine(result);
