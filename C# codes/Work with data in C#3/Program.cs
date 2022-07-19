fcb5a5a232220d948302418077d8214a24782e9e
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-19 19:45:20 +0800

    Unit 4

--- C# codes/Work with data in C#3/Program.cs
+++ C# codes/Work with data in C#3/Program.cs
@@ -11,7 +11,7 @@ Console.WriteLine($"Measurement: {measurement:N} units");
 Console.WriteLine($"Measurement: {measurement:N4} units");
 decimal tax = .36785m;
 Console.WriteLine($"Tax rate: {tax:P3}");*/
-/*Unit 3*/
+/*Unit 3
 int invoiceNumber = 1201;
 decimal productMeasurement = 25.4568m;
 decimal subtotal = 2750.00m;
@@ -22,4 +22,20 @@ Console.WriteLine($"Invoice Number: {invoiceNumber}");
 Console.WriteLine($"   Measurement: {productMeasurement:N3} mg");
 Console.WriteLine($"     Sub Total: {subtotal:C}");
 Console.WriteLine($"           Tax: {taxPercentage:P2}");
-Console.WriteLine($"     Total Due: {total:C}");
+Console.WriteLine($"     Total Due: {total:C}");*/
+/*Unit 4*/
+string input = "Pad this";
+Console.WriteLine(input.PadLeft(12));
+Console.WriteLine(input.PadRight(12));
+Console.WriteLine(input.PadLeft(12, '-'));
+Console.WriteLine(input.PadRight(12, '-'));
+string paymentId = "769";
+string payeeName = "Mr. Stephen Ortega";
+string paymentAmount = "$5,000.00";
+
+var formattedLine = paymentId.PadRight(6);
+formattedLine += payeeName.PadRight(24);
+formattedLine += paymentAmount.PadLeft(10);
+
+Console.WriteLine("1234567890123456789012345678901234567890");
+Console.WriteLine(formattedLine);
