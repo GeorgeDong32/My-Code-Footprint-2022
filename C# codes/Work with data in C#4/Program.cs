67cdf20da520b3fea469a3c0ab972c9d2ccfffa4
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-19 21:52:18 +0800

    U2 new

--- C# codes/Work with data in C#4/Program.cs
+++ C# codes/Work with data in C#4/Program.cs
@@ -4,8 +4,7 @@ string message = "Find what is (inside the parentheses)";
 int openingPosition = message.IndexOf('(');
 int closingPosition = message.IndexOf(')');
 
-Console.WriteLine(openingPosition);
-Console.WriteLine(closingPosition);
+openingPosition += 1;
 
 int length = closingPosition - openingPosition;
 Console.WriteLine(message.Substring(openingPosition, length));
