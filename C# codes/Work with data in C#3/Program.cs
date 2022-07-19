b0c8ec6b760ba3500fd3a5f02aa5a3cfd18c5ca6
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-19 18:23:19 +0800

    U3

--- C# codes/Work with data in C#3/Program.cs
+++ C# codes/Work with data in C#3/Program.cs
@@ -1,6 +1,25 @@
-﻿/*Unit 2*/
+﻿/*Unit 2
 string first = "Hello";
 string second = "World";
-string result = string.Format("{0} {1}!", first, second);
-Console.WriteLine(result);
+Console.WriteLine("{1} {0}!", first, second);
+Console.WriteLine("{0} {0} {0}!", first, second);
+decimal price = 123.45m;
+int discount = 50;
+Console.WriteLine($"Price: {price:C} (Save {discount:C})");
+decimal measurement = 123456.78912m;
+Console.WriteLine($"Measurement: {measurement:N} units");
+Console.WriteLine($"Measurement: {measurement:N4} units");
+decimal tax = .36785m;
+Console.WriteLine($"Tax rate: {tax:P3}");*/
 /*Unit 3*/
+int invoiceNumber = 1201;
+decimal productMeasurement = 25.4568m;
+decimal subtotal = 2750.00m;
+decimal taxPercentage = .15825m;
+decimal total = 3185.19m;
+
+Console.WriteLine($"Invoice Number: {invoiceNumber}");
+Console.WriteLine($"   Measurement: {productMeasurement:N3} mg");
+Console.WriteLine($"     Sub Total: {subtotal:C}");
+Console.WriteLine($"           Tax: {taxPercentage:P2}");
+Console.WriteLine($"     Total Due: {total:C}");
