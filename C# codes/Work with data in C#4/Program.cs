e13619cc0673ed4a0d141958d86440cab04713d3
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-19 21:27:53 +0800

    U2

--- C# codes/Work with data in C#4/Program.cs
+++ C# codes/Work with data in C#4/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Unit 2*/
+string message = "Find what is (inside the parentheses)";
+
+int openingPosition = message.IndexOf('(');
+int closingPosition = message.IndexOf(')');
+
+Console.WriteLine(openingPosition);
+Console.WriteLine(closingPosition);
+
+int length = closingPosition - openingPosition;
+Console.WriteLine(message.Substring(openingPosition, length));
