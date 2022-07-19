b88341decbf8405115ac2c705000806dcab6b372
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-19 23:56:28 +0800

    U2 new

--- C# codes/Work with data in C#4/Program.cs
+++ C# codes/Work with data in C#4/Program.cs
@@ -1,5 +1,5 @@
 ﻿/*Unit 2*/
-string message = "Find what is (inside the parentheses)";
+/*string message = "Find what is (inside the parentheses)";
 
 int openingPosition = message.IndexOf('(');
 int closingPosition = message.IndexOf(')');
@@ -7,4 +7,48 @@ int closingPosition = message.IndexOf(')');
 openingPosition += 1;
 
 int length = closingPosition - openingPosition;
-Console.WriteLine(message.Substring(openingPosition, length));
+Console.WriteLine(message.Substring(openingPosition, length));*/
+
+string message = "(What if) I have [different symbols] but every {open symbol} needs a [matching closing symbol]?";
+
+// The IndexOfAny() helper method requires a char array of characters. 
+// We want to look for:
+char[] openSymbols = { '[', '{', '(' };
+
+// We'll use a slightly different technique for iterating through the 
+// characters in the string. This time, we'll use the closing position
+// of the previous iteration as the starting index for the next open
+// symbol. So, we need to initialize the closingPosition variable
+// to zero:
+int closingPosition = 0;
+
+while (true)
+{
+    int openingPosition = message.IndexOfAny(openSymbols, closingPosition);
+    if (openingPosition == -1) break;
+    string currentSymbol = message.Substring(openingPosition, 1);
+
+    // Now we must find the matching closing symbol
+    char matchingSymbol = ' ';
+    switch (currentSymbol)
+    {
+        case "[":
+            matchingSymbol = ']';
+            break;
+        case "{":
+            matchingSymbol = '}';
+            break;
+        case "(":
+            matchingSymbol = ')';
+            break;
+    }
+
+    // To find the closingPosition, we use an overload of the IndexOf method to specify 
+    // that our search for the matchingSymbol should start at the openingPosition in the string. 
+    openingPosition += 1;
+    closingPosition = message.IndexOf(matchingSymbol, openingPosition);
+
+    // Finally, use the techniques we've already learned to display the sub-string:
+    int length = closingPosition - openingPosition;
+    Console.WriteLine(message.Substring(openingPosition, length));
+}
