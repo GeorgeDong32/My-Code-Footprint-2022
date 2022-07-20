8c14ac36fe0b7150d7212b3bd277f694ae1d289b
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-20 23:09:40 +0800

    U2 5

--- C# codes/Add logic to your applications with C#4/csharp-for.cs
+++ C# codes/Add logic to your applications with C#4/csharp-for.cs
@@ -3,3 +3,13 @@ for (int i = 10; i >= 0; i--)
 {
     Console.WriteLine(i);
 }
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+    if (i == 7) break;
+}
+string[] names = { "Alex", "Eddie", "David", "Michael" };
+for (int i = names.Length - 1; i >= 0; i--)
+{
+    Console.WriteLine(names[i]);
+}
