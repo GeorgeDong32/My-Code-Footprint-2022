35d46f5354f39a019df5a1342e4dcd427fc58688
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-20 19:48:34 +0800

    Finish Challenge

--- C# codes/Add logic to your applications with C#2/Program.cs
+++ C# codes/Add logic to your applications with C#2/Program.cs
@@ -23,17 +23,14 @@ namespace MyNewApp
 /*Unit 4*/
 /*Challenge*/
 int[] numbers = { 4, 8, 15, 16, 23, 42 };
+bool found = false;
+int total = 0;
 foreach (int number in numbers)
 {
-    int total;
     total += number;
     if (number == 42)
-    {
-        bool found = true;
-    }
+        found = true;
 }
 if (found)
-{
     Console.WriteLine("Set contains 42");
-}
 Console.WriteLine($"Total: {total}");
