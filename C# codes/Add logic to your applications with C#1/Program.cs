30eff2355c71da17163107b0e7328e6b672b5ca7
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-20 17:26:30 +0800

    U2

--- C# codes/Add logic to your applications with C#1/Program.cs
+++ C# codes/Add logic to your applications with C#1/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Unit 2*/
+bool flag = true;
+if (flag)
+{
+    int value = 10;
+    Console.WriteLine($"Inside of code block: {value}");
+}
