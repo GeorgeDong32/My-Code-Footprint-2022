9ea113e096c1a89873fca73df23cbaf06b1b2c53
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-20 19:45:41 +0800

    init challenge

--- C# codes/Add logic to your applications with C#2/Program.cs
+++ C# codes/Add logic to your applications with C#2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿/*Unit 3
+using System;
 
 namespace MyNewApp
 {
@@ -18,4 +19,21 @@ namespace MyNewApp
             return new string(letters);
         }
     }
+}*/
+/*Unit 4*/
+/*Challenge*/
+int[] numbers = { 4, 8, 15, 16, 23, 42 };
+foreach (int number in numbers)
+{
+    int total;
+    total += number;
+    if (number == 42)
+    {
+        bool found = true;
+    }
 }
+if (found)
+{
+    Console.WriteLine("Set contains 42");
+}
+Console.WriteLine($"Total: {total}");
