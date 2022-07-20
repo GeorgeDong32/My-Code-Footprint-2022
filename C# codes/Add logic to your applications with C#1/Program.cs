7b4cd8f666667e4463d5c2013d9f13c56519400b
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-20 18:36:33 +0800

    Challenge 2

--- C# codes/Add logic to your applications with C#1/Program.cs
+++ C# codes/Add logic to your applications with C#1/Program.cs
@@ -5,7 +5,7 @@ Console.WriteLine(1 == 2);
 
 string myValue = "a";
 Console.WriteLine(myValue == "a");*/
-/*Challenge*/
+/*Challenge 1
 Random coin = new Random();
 int result = coin.Next(0, 2);
 if (result == 1)
@@ -18,4 +18,19 @@ else
 }
 //
 //Random coin = new Random();
-Console.WriteLine((coin.Next(0, 2) == 0) ? "heads" : "tails");
+Console.WriteLine((coin.Next(0, 2) == 0) ? "heads" : "tails");*/
+/*Challenge 2*/
+string permission = "Admin|Manager";
+int level = 55;
+if (permission.Contains("Admin"))
+{
+    Console.WriteLine((level > 55) ? "Welcome, Super Admin user." : "Welcome, Admin user.");
+}
+else if (permission.Contains("Manager"))
+{
+    Console.WriteLine((level >= 20) ? "Contact an Admin for access." : "You do not have sufficient privileges.");
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
