b0c620cbebe7d3dce8fb770c4f9b0371719a9e91
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-20 17:39:08 +0800

    Challenge

--- C# codes/Add logic to your applications with C#1/Program.cs
+++ C# codes/Add logic to your applications with C#1/Program.cs
@@ -1,7 +1,21 @@
-﻿/*Unit 2*/
+﻿/*Unit 2
 Console.WriteLine("a" == "a");
 Console.WriteLine("a" == "A");
 Console.WriteLine(1 == 2);
 
 string myValue = "a";
-Console.WriteLine(myValue == "a");
+Console.WriteLine(myValue == "a");*/
+/*Challenge*/
+Random coin = new Random();
+int result = coin.Next(0, 2);
+if (result == 1)
+{
+    Console.WriteLine("heads");
+}
+else
+{
+    Console.WriteLine("tails");
+}
+//
+Random coin = new Random();
+Console.WriteLine((coin.Next(0, 2) == 0) ? "heads" : "tails");
