22be9dfa7d261c53be0270ef969cda8e16d26b63
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-20 20:07:01 +0800

    Unit 2

--- C# codes/Add logic to your applications with C#3/Program.cs
+++ C# codes/Add logic to your applications with C#3/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Unit 2*/
+int employeeLevel = 100;
+string employeeName = "John Smith";
+
+string title = "";
+
+switch (employeeLevel)
+{
+    case 100:
+    case 200:
+        title = "Senior Associate";
+        break;
+    case 300:
+        title = "Manager";
+        break;
+    case 400:
+        title = "Senior Manager";
+        break;
+    default:
+        title = "Associate";
+        break;
+}
+
+Console.WriteLine($"{employeeName}, {title}");
+/*Challenge*/
