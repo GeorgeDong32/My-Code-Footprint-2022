aeade8162901812e12f8e6c3c6d7c0aa2ac335d7
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-20 23:21:56 +0800

    Challenge

--- C# codes/Add logic to your applications with C#4/csharp-for.cs
+++ C# codes/Add logic to your applications with C#4/csharp-for.cs
@@ -1,4 +1,4 @@
-/*Unit 2*/
+/*Unit 2
 for (int i = 10; i >= 0; i--)
 {
     Console.WriteLine(i);
@@ -14,8 +14,8 @@ for (int i = names.Length - 1; i >= 0; i--)
     Console.WriteLine(names[i]);
 }
 //string[] names = { "Alex", "Eddie", "David", "Michael" };
-foreach (var name in names)
-{
-    // Can't do this:
-    if (name == "David") name = "Sammy";
-}
+for (int i = 0; i < names.Length; i++)
+    if (names[i] == "David") names[i] = "Sammy";
+
+foreach (var name in names) Console.WriteLine(name);*/
+/*Challenge*/
