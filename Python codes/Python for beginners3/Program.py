26964eb3cb8945b774b4596ba0c9a5e854af1e61
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-22 22:28:00 +0800

    date

--- Python codes/Python for beginners3/Program.py
+++ Python codes/Python for beginners3/Program.py
@@ -1,6 +1,14 @@
 # program.py
+from datetime import date
 sum = 1 + 2
 print(sum)
 product = sum * 2
 print("show this in the console")
 print(product)
+# data type
+planets_in_solar_system = 8  #  int, pluto used to be the 9th planet, but is too small
+distance_to_alpha_centauri = 4.367  #  float, lightyears
+can_liftoff = True
+shuttle_landed_on_the_moon = "Apollo 11"  # string
+# date
+print(date.today())
