dc4ccd9b99acd11e16032a736226aabb2a0e2f46
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-23 22:22:20 +0800

    Test

--- Python codes/Python for beginners3/Program.py
+++ Python codes/Python for beginners3/Program.py
@@ -26,3 +26,6 @@ first_number = input("first number: ")
 second_number = input("second number: ")
 print(first_number + second_number)
 print(int(first_number) + int(second_number))
+
+# Test
+print("1" + 2)
