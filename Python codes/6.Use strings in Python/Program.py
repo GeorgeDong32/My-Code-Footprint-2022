409ef185f31200373eb1ca4e3b2bc90150069b38
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-25 21:59:53 +0800

    U5 1

---
+++ Python codes/6.Use strings in Python/Program.py
@@ -0,0 +1,8 @@
+# string format
+# %
+print("""Both sides of the %s get the same amount of sunlight,
+but only one side is seen from %s because
+the %s rotates around its own axis when it orbits %s.""" % ("Moon", "Earth", "Moon", "Earth"))
+# .format()
+
+# f-string
