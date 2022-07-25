47ea9f32900b59bef522e2074262fccc8d70452c
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-25 22:01:31 +0800

    U5 2

--- Python codes/6.Use strings in Python/Program2.py
+++ Python codes/6.Use strings in Python/Program2.py
@@ -4,5 +4,7 @@ print("""Both sides of the %s get the same amount of sunlight,
 but only one side is seen from %s because
 the %s rotates around its own axis when it orbits %s.""" % ("Moon", "Earth", "Moon", "Earth"))
 # .format()
-
+mass_percentage = "1 / 6"
+print("""You are lighter on the {0}, because on the {0} 
+	you would weigh about {1} of your weight on Earth""".format("Moon", mass_percentage))
 # f-string
