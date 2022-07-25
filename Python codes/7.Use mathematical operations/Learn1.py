2dd77695510e43283f5b96860acfdbed38d8d7d6
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-25 23:37:46 +0800

    end learn1

--- Python codes/7.Use mathematical operations/Learn1.py
+++ Python codes/7.Use mathematical operations/Learn1.py
@@ -14,3 +14,12 @@ print(f"mult = {product}")
 # division
 quotient = 30 / 12
 print(f"division = {quotient}")
+
+seconds = 1042
+display_minutes = 1042 / 60
+print(display_minutes)
+display_minutes = 1042 // 60
+display_seconds = 1042 % 60
+
+print(display_minutes)
+print(display_seconds)
