1c49f1e15ae8644f66fb8dc4b0f66be3e64e1516
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-25 23:57:05 +0800

    Learn2

---
+++ Python codes/7.Use mathematical operations/Learn2.py
@@ -0,0 +1,11 @@
+# use number in py
+demo_int = int('215')
+print(demo_int)
+
+demo_float = float('215.3')
+print(demo_float)
+# abs
+print(abs(39 - 16))
+print(abs(16 - 39))
+# round
+print(round(14.5))
