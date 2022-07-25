0b2e4c5a647c3ae51c630469ad71b084b8891fdf
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-25 11:32:54 +0800

    U2

--- Python codes/Python for beginners5/Program.py
+++ Python codes/Python for beginners5/Program.py
@@ -1,1 +1,15 @@
 # Print warning messsage
+# test if
+a = 97
+b = 55
+# test expression
+if a < b:
+    print("b > a")
+else:
+    print("a > b")
+# test
+a = 24
+b = 44
+if a <= 0:
+    print(a)
+print(b)
