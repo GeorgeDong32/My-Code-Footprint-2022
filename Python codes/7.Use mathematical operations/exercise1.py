0546c614dcb39dc96c2626c0d0eb72b3870d8318
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-25 23:42:44 +0800

    exercise1

---
+++ Python codes/7.Use mathematical operations/exercise1.py
@@ -0,0 +1,8 @@
+# Arithmetic operators in Python
+first_planet = 149597870
+second_planet = 778547200
+distance_km = second_planet - first_planet
+print(distance_km)
+
+distance_mi = distance_km / 1.609344
+print(distance_mi)
