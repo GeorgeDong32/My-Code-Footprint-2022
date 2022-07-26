f568f5cf95e1a89610ccfce421f1303099cd20b4
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-26 23:34:58 +0800

    Learn2

---
+++ Python codes/9.Use loops in Python/Learn2.py
@@ -0,0 +1,15 @@
+# for
+# countdown
+from time import sleep
+countdown = [4, 3, 2, 1, 0]
+for number in countdown:
+    print(number)
+print("Blast off!! 🚀")
+
+# realcountdown
+countdown = [4, 3, 2, 1, 0]
+
+for number in countdown:
+    print(number)
+    sleep(1)  # Wait 1 second
+print("Blast off!! 🚀")
