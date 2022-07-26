c27b33c0f4da2dd723b140db215e8fea10fd57ef
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-26 22:00:50 +0800

    切片列表

--- Python codes/8.Introduction to lists in Python/Learn3.py
+++ Python codes/8.Introduction to lists in Python/Learn3.py
@@ -1,1 +1,14 @@
 # 操作列表数据
+# 切片列表
+planets = ["Mercury", "Venus", "Earth", "Mars",
+           "Jupiter", "Saturn", "Uranus", "Neptune"]
+planets_before_earth = planets[0:2]
+print(planets_before_earth)
+# Output: ['Mercury', 'Venus']
+
+planets_after_earth = planets[3:8]
+print(planets_after_earth)
+# Output
+# ['Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+
+# 联接列表
