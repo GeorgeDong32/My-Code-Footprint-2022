824fea46a09d07332936e245dc2f6f3fc1184e58
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-26 20:34:48 +0800

    2

--- Python codes/8.Introduction to lists in Python/Learn2.py
+++ Python codes/8.Introduction to lists in Python/Learn2.py
@@ -1,3 +1,14 @@
 # 使用列表中的数字
 gravity_on_earth = 1.0
 gravity_on_the_moon = 0.166
+
+gravity_on_planets = [0.378, 0.907, 1, 0.377, 2.36, 0.916, 0.889, 1.12]
+# calculation
+bus_weight = 12650  # in kilograms, on Earth
+
+print("On Earth, a double-decker bus weighs", bus_weight, "kg")
+print("On Mercury, a double-decker bus weighs",
+      bus_weight * gravity_on_planets[0], "kg")
+# Output
+# On Earth, a double-decker bus weighs 12650 kg
+# On Mercury, a double-decker bus weighs 4781.7 kg
