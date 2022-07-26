f63a434b7dee17f532f64423dba9b3fb100c0f02
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-26 22:15:40 +0800

    Finish

--- Python codes/8.Introduction to lists in Python/Exercise2.py
+++ Python codes/8.Introduction to lists in Python/Exercise2.py
@@ -3,5 +3,7 @@ planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Neptune"]
 user_planet = input(
     "Please enter the name of the planet (with a capital letter to start)")
 planet_index = planets.index(user_planet)
-print("Here are the planets that close to the sun than your select:")
+print("Here are the planets closer than " + user_planet)
 print(planets[0:planet_index])
+print("Here are the planets further than " + user_planet)
+print(planets[planet_index + 1:])
