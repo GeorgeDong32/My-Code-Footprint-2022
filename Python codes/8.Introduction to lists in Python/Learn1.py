41c1dff50938eebf9bbc3c24dd1cc0974eaf2979
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-26 15:36:31 +0800

    Learn1

---
+++ Python codes/8.Introduction to lists in Python/Learn1.py
@@ -0,0 +1,36 @@
+# python list
+# create
+
+planets = ["Mercury", "Venus", "Earth", "Mars",
+           "Jupiter", "Saturn", "Uranus", "Neptune"]
+print("The first planet is", planets[0])
+print("The second planet is", planets[1])
+print("The third planet is", planets[2])
+# Output
+# The first planet is Mercury
+# The second planet is Venus
+# The third planet is Earth
+
+planets[3] = "Red Planet"
+print("Mars is also known as", planets[3])
+# Output
+# Mars is also known as Red Planet
+
+
+# method
+
+number_of_planets = len(planets)
+print("There are", number_of_planets, "planets in the solar system.")
+# Output:
+# There are 8 planets in the solar system
+
+planets.append("Pluto")
+number_of_planets = len(planets)
+print("There are actually", number_of_planets, "planets in the solar system.")
+# Output:
+# There are actually 9 planets in the solar system.
+
+planets.pop()  # Goodbye, Pluto
+number_of_planets = len(planets)
+print("No, there are definitely", number_of_planets,
+      "planets in the solar system.")
