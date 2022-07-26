8514e8bf47bfb68cb2b501a8cfeb3f29e9ff2ff5
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-26 23:46:55 +0800

    Init exercise

---
+++ Python codes/10.Python dictionaries/3-exercise-create-modify.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Exercise: Create and modify a Python dictionary
+# 
+# Python dictionaries allow you to model more complex data. Dictionaries are a collection of key/value pairs, and are very common in Python programs. Their flexibility allows you to dynamically work with related values without having to create classes or objects.
+# 
+# This exercise is broken into a series of steps. For each step you will be presented with the goal for the step, followed by an empty cell. Enter your Python into the cell and run it. The solution for each step will follow each cell.
+# 
+# ## Managing planet data
+# 
+# You want to create a program which will store and display information about planets. To start you will use one planet. Create a variable named `planet`. Store the following values as a dictionary:
+# 
+# ```
+# name: Mars
+# moons: 2
+# ```
+
+# In[ ]:
+
+
+# Enter code below
+
+
+# Your code should look like the following:
+# 
+# ```python
+# planet = {
+#     'name': 'Mars',
+#     'moons': 2
+# }
+# ```
+# 
+# ## Display planet data
+# 
+# With the variable created, you will now display information. You can retrieve information by either using `get` or square brackets (`[ ]`) and the key name. Add the code to display the planet information in the following format:
+# 
+# **__ has __ moon(s)**
+# 
+# If you were working with Earth, the output would be **Earth has 1 moon(s)**
+# 
+# > **Note:** You can use whatever string formatting option you like
+
+# In[ ]:
+
+
+# Enter code below
+
+
+# Your code should look like the following:
+# 
+# ```python
+# print(f'{planet["name"]} has {planet["moons"]} moon(s)')
+# ```
+# 
+# ## Desired output
+# 
+# When you run the cell you should see the following result:
+# 
+# ```
+# Mars has 2 moon(s)
+
+# ## Add circumference information
+# 
+# You can update existing keys or create new ones by either using the `update` method or using square brackets (`[ ]`). When you're using `update`, you pass in a new dictionary object with the updated or new values. When using square brackets, you specify the key name and assign a new value.
+# 
+# Add a new value to `planet` with a key of `'circumference (km)'`. This new value should store a dictionary with the planet's two circumferences:
+# 
+# ```
+# polar: 6752
+# equatorial: 6792
+# ```
+# 
+# Finally, add the code to print the polar circumference of the planet. You can use whatever sentence formatting you wish.
+
+# In[ ]:
+
+
+# Enter code below
+
+
+# Your code should look like the following:
+# 
+# ```python
+# planet['circumference (km)'] = {
+#     'polar': 6752,
+#     'equatorial': 6792
+# }
+# 
+# print(f'{planet["name"]} has a polar circumference of {planet["circumference (km)"]["polar"]}')
+# ```
+# 
+# ## Desired output
+# 
+# When you run the cell you should see the following result:
+# 
+# ```
+# Mars has a polar circumference of 6752
---
+++ Python codes/10.Python dictionaries/5-exercise-dynamic-programming.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Calculating values
+# 
+# In this scenario, you will calculate both the total number of moons in the solar system and the average number of moons a planet has. You will do this by using a dictionary object.
+# 
+# This exercise is broken into a series of steps. For each step you will be presented with the goal for the step, followed by an empty cell. Enter your Python into the cell and run it. The solution for each step will follow each cell.
+# 
+# Start by creating a variable named `planet_moons` as a dictionary with the following key/values:
+# 
+# ```
+# mercury: 0,
+# venus: 0,
+# earth: 1,
+# mars: 2,
+# jupiter: 79,
+# saturn: 82,
+# uranus: 27,
+# neptune: 14,
+# pluto: 5,
+# haumea: 2,
+# makemake: 1,
+# eris: 1
+# ```
+
+# In[ ]:
+
+
+# Enter the code below
+
+
+# Your code should look like the following:
+# 
+# ```python
+# planet_moons = {
+#     'mercury': 0,
+#     'venus': 0,
+#     'earth': 1,
+#     'mars': 2,
+#     'jupiter': 79,
+#     'saturn': 82,
+#     'uranus': 27,
+#     'neptune': 14,
+#     'pluto': 5,
+#     'haumea': 2,
+#     'makemake': 1,
+#     'eris': 1
+# }
+# ```
+
+# ## Obtain a list of moons and number of planets
+# 
+# Python dictionaries allow you to retrieve all the values and keys by using the `values` and `keys` methods, respectively. Each method returns a list containing the data, which can then be used like a regular Python list. You can determine the number of items by using `len`, and iterate through it by using `for` loops. In the dictionary you created, the planet names are keys and the number of moons are the values.
+# 
+# Start by retrieving a list with the number of moons, and store this in a variable named `moons`. Then obtain the total number of planets and store that value in a variable named `total_planets`.
+
+# In[ ]:
+
+
+# Enter code below
+
+
+# Your code should look like the following:
+# 
+# ```python
+# moons = planet_moons.values()
+# total_planets = len(planet_moons.keys())
+# ```
+
+# ## Determine the average number of moons
+# 
+# You will finish this exercise by determining the average number of moons. Start by creating a variable named `total_moons`; this will be your counter for the total number of moons. Then add a `for` loop to loop through the list of `moons`, adding each value to `total_moons`. Finally, calculate the average by dividing `total_moons` by `total_planets` and displaying the value.
+
+# In[ ]:
+
+
+# Enter code below
+
+
+# Your code should look like the following:
+# 
+# ```python
+# total_moons = 0
+# for moon in moons:
+#     total_moons = total_moons + moon
+# 
+# average = total_moons / total_planets
+# 
+# print(f'Each planet as an average of {average} moons')
+# ````
+# 
+# ## Desired output
+# 
+# When you run the notebook you should see the following result:
+# 
+# ```
+# Each planet as an average of 17.833333333333332 moons
+# ```
