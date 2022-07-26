175040b97d660aa78bba8741db1bfd6469cf5722
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-26 20:48:16 +0800

    min n max

--- Python codes/8.Introduction to lists in Python/Learn2.py
+++ Python codes/8.Introduction to lists in Python/Learn2.py
@@ -12,3 +12,16 @@ print("On Mercury, a double-decker bus weighs",
 # Output
 # On Earth, a double-decker bus weighs 12650 kg
 # On Mercury, a double-decker bus weighs 4781.7 kg
+
+# min & max
+bus_weight = 12650  # in kilograms, on Earth
+
+print("On Earth, a double-decker bus weighs", bus_weight, "kg")
+print("The lightest a bus would be in the solar system is",
+      bus_weight * min(gravity_on_planets), "kg")
+print("The heaviest a bus would be in the solar system is",
+      bus_weight * max(gravity_on_planets), "kg")
+# Output
+# On Earth, a double-decker bus weighs 12650 kg
+# The lightest a bus would be in the solar system is 4769.05 kg
+# The heaviest a bus would be in the solar system is 29854 kg
