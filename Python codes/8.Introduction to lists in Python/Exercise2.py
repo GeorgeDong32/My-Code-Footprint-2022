0f5d53d5b82cb1b72f4d19b7b2dd78deefac5785
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-26 22:14:02 +0800

    Finish

--- Python codes/8.Introduction to lists in Python/Exercise2.py
+++ Python codes/8.Introduction to lists in Python/Exercise2.py
@@ -1,2 +1,7 @@
 # Use slices to retrieve portions of a list
 planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Neptune"]
+user_planet = input(
+    "Please enter the name of the planet (with a capital letter to start)")
+planet_index = planets.index(user_planet)
+print("Here are the planets that close to the sun than your select:")
+print(planets[0:planet_index])
