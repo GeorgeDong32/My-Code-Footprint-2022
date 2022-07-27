824a15abf1024d80b4b53feec2cee83e122eb212
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-28 00:12:28 +0800

    Refine waterleft

--- Python codes/12.Python error handling/引发异常/Learn3.py
+++ Python codes/12.Python error handling/引发异常/Learn3.py
@@ -1,4 +1,13 @@
 def water_left(astronauts, water_left, days_left):
+    for argument in [astronauts, water_left, days_left]:
+        try:
+            # If argument is an int, the following operation will work
+            argument / 10
+        except TypeError:
+            # TypError will be raised only if it isn't the right type
+            # Raise the same exception but with a better error message
+            raise TypeError(
+                f"All arguments must be of type int, but received: '{argument}'")
     daily_usage = astronauts * 11
     total_usage = daily_usage * days_left
     total_water_left = water_left - total_usage
@@ -8,4 +17,7 @@ def water_left(astronauts, water_left, days_left):
     return f"Total water left after {days_left} days is: {total_water_left} liters"
 
 
-water_left(5, 100, 2)
+try:
+    water_left(5, 100, 2)
+except RuntimeError as err:
+    alert_navigation_system(err)
