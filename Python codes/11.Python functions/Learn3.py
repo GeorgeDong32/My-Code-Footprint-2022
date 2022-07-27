99173d539d9a7d788624fc87a1a1a946f3b82c91
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-27 21:58:36 +0800

    Learn3

--- Python codes/11.Python functions/Learn3.py
+++ Python codes/11.Python functions/Learn3.py
@@ -6,3 +6,16 @@ def arrival_time(hours=51):
     now = datetime.now()
     arrival = now + timedelta(hours=hours)
     return arrival.strftime("Arrival: %A %H:%M")
+
+
+print(arrival_time())
+
+
+def arrival_time(destination, hours=51):
+    now = datetime.now()
+    arrival = now + timedelta(hours=hours)
+    return arrival.strftime(f"{destination} Arrival: %A %H:%M")
+
+
+print(arrival_time("Moon"))
+print(arrival_time("Orbit", hours=0.13))
