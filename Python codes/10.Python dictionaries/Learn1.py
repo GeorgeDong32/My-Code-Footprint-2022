5aceadf8b78dbb2b8ad1f668191148c2af284368
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-27 14:42:32 +0800

    update

--- Python codes/10.Python dictionaries/Learn1.py
+++ Python codes/10.Python dictionaries/Learn1.py
@@ -11,3 +11,18 @@ print(planet.get('name'))
 # planet['name'] is identical to using planet.get('name')
 print(planet['name'])
 # Displays Earth
+
+# update
+# Using update
+planet.update({
+    'name': 'Jupiter',
+    'moons': 79
+})
+print(planet['name'])
+print(planet['moons'])
+
+# Using square brackets
+planet['name'] = 'Jupiter'
+planet['moons'] = 79
+print(planet['name'])
+print(planet['moons'])
