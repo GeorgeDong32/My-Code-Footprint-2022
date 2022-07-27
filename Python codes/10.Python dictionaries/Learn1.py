8d928de98f1592c71e96462d710807a97de84caf
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-27 14:14:44 +0800

    get

--- Python codes/10.Python dictionaries/Learn1.py
+++ Python codes/10.Python dictionaries/Learn1.py
@@ -1,1 +1,13 @@
 # python 字典
+planet = {
+    'name': 'Earth',
+    'moons': 1
+}
+
+# get
+print(planet.get('name'))
+# Displays Earth
+
+# planet['name'] is identical to using planet.get('name')
+print(planet['name'])
+# Displays Earth
