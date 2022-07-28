ae88142ea4bfbfac75a06be727afc84c706812eb
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-28 18:02:13 +0800

    New function

--- Python codes/12.Python error handling/处理异常/config.py
+++ Python codes/12.Python error handling/处理异常/config.py
@@ -1,8 +1,10 @@
 def main():
     try:
         configuration = open('config.txt')
-    except Exception:
+    except FileNotFoundError:
         print("Couldn't find the config.txt file!")
+    except IsADirectoryError:
+        print("Found config.txt but it is a directory, couldn't read it")
 
 
 if __name__ == '__main__':
