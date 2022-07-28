006127b1423824e876e57f30052a62cccdd55b9f
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-28 20:31:15 +0800

    ex3

--- Python codes/Crack the code and reveal a secret with Python and Visual Studio Code/Sleuth/decrypt.py
+++ Python codes/Crack the code and reveal a secret with Python and Visual Studio Code/Sleuth/decrypt.py
@@ -1,3 +1,4 @@
+'''
 print("Hello, Contosoville!")
 # This is a comment that won't be interpreted as a command.
 
@@ -17,3 +18,15 @@ def chant(phrase):
 
 # Invoke the power to chant on the phrase "Contosoville!"
 chant("Contosoville!")
+'''
+
+
+def lasso_letter(letter, shift_amount):
+    letter_code = ord(letter.lower())
+    decoded_letter_code = letter_code + shift_amount
+    decoded_letter = chr(decoded_letter_code)
+    return decoded_letter
+
+
+print(lasso_letter('a', 2))
+print(lasso_letter('N', 13))
