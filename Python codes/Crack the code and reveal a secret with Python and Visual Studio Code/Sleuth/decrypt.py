7d9c4b587bd0592e182952724bd6911e34dd8be1
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-28 19:16:49 +0800

    ex2

--- Python codes/Crack the code and reveal a secret with Python and Visual Studio Code/Sleuth/decrypt.py
+++ Python codes/Crack the code and reveal a secret with Python and Visual Studio Code/Sleuth/decrypt.py
@@ -1,1 +1,19 @@
-print( "Hello, Contosoville!" )
+print("Hello, Contosoville!")
+# This is a comment that won't be interpreted as a command.
+
+# Associate variable town with the value "Contosoville"
+town = "Contosoville"
+
+# Print a message about the secret location
+print("The town I am looking for is " + town)
+
+# Define a power (function) to chant a phrase
+
+
+def chant(phrase):
+    # Glue three copies together and print it as a message
+    print(phrase + phrase + phrase)
+
+
+# Invoke the power to chant on the phrase "Contosoville!"
+chant("Contosoville!")
