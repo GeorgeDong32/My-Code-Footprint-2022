c1ef00573cbec83e2c4abf60f14d69665409680a
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-28 20:41:04 +0800

    ex4

--- Python codes/Crack the code and reveal a secret with Python and Visual Studio Code/Sleuth/decrypt.py
+++ Python codes/Crack the code and reveal a secret with Python and Visual Studio Code/Sleuth/decrypt.py
@@ -21,10 +21,27 @@ chant("Contosoville!")
 '''
 
 
+# Define a function to find the truth by shifting the letter by the specified amount
 def lasso_letter(letter, shift_amount):
+    # Invoke the ord function to translate the letter to its ASCII code
+    # Save the code to the letter_code variable
     letter_code = ord(letter.lower())
-    decoded_letter_code = letter_code + shift_amount
-    decoded_letter = chr(decoded_letter_code)
+
+    # The ASCII number representation of lowercase letter 'a'
+    a_ascii = ord('a')
+
+    # The number of letters in the alphabet
+    alphabet_size = 26
+
+    # The formula to calculate the ASCII number for the decoded letter
+    # Take into account looping around the alphabet
+    true_letter_code = a_ascii + \
+        (((letter_code - a_ascii) + shift_amount) % alphabet_size)
+
+    # Convert the ASCII number to the character or letter
+    decoded_letter = chr(true_letter_code)
+
+    # Send the decoded letter back
     return decoded_letter
 
 
