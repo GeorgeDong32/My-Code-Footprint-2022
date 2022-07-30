af6d33b5b2a57d0ac338e98c038aa3ea9aed1c5b
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-30 19:26:47 +0800

    End test

--- Python codes/Build real app with Python/Get started with testing in Python/test_exercise.py
+++ Python codes/Build real app with Python/Get started with testing in Python/test_exercise.py
@@ -4,7 +4,10 @@ import unittest
 def str_to_bool(value):
     true_values = ['y', 'yes']
     false_values = ['no', 'n']
-
+    try:
+        value = value.lower()
+    except AttributeError:
+        raise AttributeError(f"{value} must be of type string")
     if value in true_values:
         return True
     if value in false_values:
@@ -20,6 +23,10 @@ class TestStrToBool(unittest.TestCase):
     def test_yes_is_true(self):
         result = str_to_bool('Yes')  # 错误，Yes不属于真值列表中的一个
         self.assertTrue(result)
+
+    def test_invalid_input(self):
+        with self.assertRaises(AttributeError):
+            str_to_bool(1)
 
 
 if __name__ == '__main__':
