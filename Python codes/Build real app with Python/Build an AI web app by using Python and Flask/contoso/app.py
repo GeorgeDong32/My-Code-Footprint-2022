a8b222ced1fc9d0529343ec58b28a815a9d36849
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-30 20:10:25 +0800

    Init

---
+++ Python codes/Build real app with Python/Build an AI web app by using Python and Flask/contoso/app.py
@@ -0,0 +1,7 @@
+from flask import Flask, redirect, url_for, request, render_template, session
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
