9930c0d80bc335722243972b8616482caefc4894
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-30 14:35:40 +0800

    Update

--- Python codes/Build real app with Python/Introduction to OOP with Python/Program.py
+++ Python codes/Build real app with Python/Introduction to OOP with Python/Program.py
@@ -11,18 +11,28 @@
 # print(elevator.floor)  # 1
 
 # test _ & __
-
 class Square:
-    def __init__(self):
-        self.__height = 2
-        self.__width = 2
+    def __init__(self, w, h):
+        self.height = h
+        self.__width = w
 
     def set_side(self, new_side):
         self.__height = new_side
         self.__width = new_side
 
+    @property
+    def height(self):
+        return self.__height
 
-square = Square()
-square.__height = 3  # raises AttributeError
-print(square.__height)
-print(square.__width)
+    @height.setter
+    def height(self, new_value):
+        if new_value >= 0:
+            self.__height = new_value
+        else:
+            raise Exception("Value must be larger than 0")
+
+
+square = Square(3, 4)
+print(square.height)
+
+print(square.height)
