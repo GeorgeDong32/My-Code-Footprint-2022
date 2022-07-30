0b7659c2ec74e98b390dbd2ca62a5cdc4d3c73b7
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-30 15:02:19 +0800

    Update game class

--- Python codes/Build real app with Python/Introduction to OOP with Python/rock-paper-scissor.py
+++ Python codes/Build real app with Python/Introduction to OOP with Python/rock-paper-scissor.py
@@ -18,6 +18,9 @@ class Participant:
         }
         return switcher[self.choice]
 
+    def incrementPoint(self):
+        self.points += 1
+
 
 class GameRound:
     def __init__(self, p1, p2):
@@ -32,6 +35,10 @@ class GameRound:
         result = self.compareChoices(p1, p2)
         print("Round resulted in a {result}".format(
             result=self.getResultAsString(result)))
+        if result > 0:
+            p1.incrementPoint()
+        elif result < 0:
+            p2.incrementPoint()
 
     def compareChoices(self, p1, p2):
         return self.rules[p1.toNumericalChoice()][p2.toNumericalChoice()]
@@ -55,13 +62,30 @@ class Game:
         self.secondParticipant = Participant("Kirk")
 
     def start(self):
-        game_round = GameRound(self.participant, self.secondParticipant)
+        while not self.endGame:
+            GameRound(self.participant, self.secondParticipant)
+            self.checkEndCondition()
 
     def checkEndCondition(self):
-        print("implement")
+        answer = input("Continue game y/n: ")
+        if answer == 'y':
+            GameRound(self.participant, self.secondParticipant)
+            self.checkEndCondition()
+        else:
+            print("Game ended, {p1name} has {p1points}, and {p2name} has {p2points}".format(p1name=self.participant.name,
+                  p1points=self.participant.points, p2name=self.secondParticipant.name, p2points=self.secondParticipant.points))
+            self.determineWinner()
+            self.endGame = True
 
     def determineWinner(self):
-        print("implement")
+        resultString = "It's a Draw"
+        if self.participant.points > self.secondParticipant.points:
+            resultString = "Winner is {name}".format(
+                name=self.participant.name)
+        elif self.participant.points < self.secondParticipant.points:
+            resultString = "Winner is {name}".format(
+                name=self.secondParticipant.name)
+        print(resultString)
 
 
 game = Game()
