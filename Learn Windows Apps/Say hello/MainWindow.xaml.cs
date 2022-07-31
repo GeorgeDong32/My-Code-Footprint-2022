7cee5063d7ed62e497bc7250528cfdefd29a8f79
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-31 15:16:57 +0800

    Finish

---
+++ Learn Windows Apps/Say hello/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Say_hello
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random rand;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            rand = new Random();
+        }
+
+        private byte[] GetRandomBytes(int n)
+        {
+            //  Fill an array of bytes of length "n" with random numbers.
+            var randomBytes = new byte[n];
+            rand.NextBytes(randomBytes);
+            return randomBytes;
+        }
+
+        private void SayHelloButton_Click(object sender, RoutedEventArgs e)
+        {
+            byte[] rgb = GetRandomBytes(3);
+
+            //  Create a solid color brush using the three random numbers.
+            var randomColorBrush = new SolidColorBrush(Color.FromArgb(255, rgb[0], rgb[1], rgb[2]));
+
+            //  Set both the text color and the text box border to the random color.
+            textBox1.BorderBrush = randomColorBrush;
+            textBox1.Foreground = randomColorBrush;
+        }
+    }
+}
