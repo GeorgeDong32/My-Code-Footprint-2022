22ce623c9fc8619c18e6c8f6b6f7ffc7c0e621cd
GeorgeDong32 <georgedong32@foxmail.com>
2022-07-31 15:40:25 +0800

    Finish Win32 Build

---
+++ Learn Windows Apps/Say Hello2/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Say_Hello2
+{
+    public partial class Form1 : Form
+    {
+        Random rand;
+
+        public Form1()
+        {
+            InitializeComponent();
+            rand = new Random();
+        }
+
+        private byte[] GetRandomBytes(int n)
+        {
+            //  Fill an array of bytes of length "n" with random numbers.
+            var randomBytes = new byte[n];
+            rand.NextBytes(randomBytes);
+            return randomBytes;
+        }
+
+        private void SayHelloButton_Click(object sender, EventArgs e)
+        {
+
+            //  Declare an array of bytes and fill it with random numbers
+            byte[] rgb = GetRandomBytes(3);
+            textBox1.ForeColor = Color.FromArgb(255, rgb[0], rgb[1], rgb[2]);
+        }
+    }
+}
