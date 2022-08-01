16991ce8b551bfd6c8b8e8a2ff189eeb3870e3ba
GeorgeDong32 <georgedong32@foxmail.com>
2022-08-01 18:53:09 +0800

    Create Learn1.m

---
+++ MATLAB codes/MATLAB slearn/Learn1.m
@@ -0,0 +1,16 @@
+function importfile(fileToRead1)
+%IMPORTFILE(FILETOREAD1)
+%  从指定文件中导入数据
+%  FILETOREAD1:  要读取的文件
+
+%  由 MATLAB 于 01-Aug-2022 18:50:46 自动生成
+
+% 导入文件
+newData1 = load('-mat', fileToRead1);
+
+% 在基础工作区中从这些字段创建新变量。
+vars = fieldnames(newData1);
+for i = 1:length(vars)
+    assignin('base', vars{i}, newData1.(vars{i}));
+end
+
