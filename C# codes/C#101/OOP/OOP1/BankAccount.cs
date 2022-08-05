784177d1834039a6d6f36607bc2c209146caf8f3
GeorgeDong32 <georgedong32@foxmail.com>
2022-08-05 16:45:24 +0800

    End

--- C# codes/C#101/OOP/OOP1/BankAccount.cs
+++ C# codes/C#101/OOP/OOP1/BankAccount.cs
@@ -54,5 +54,20 @@ namespace Classes
             var withdrawal = new Transaction(-amount, date, note);
             allTransactions.Add(withdrawal);
         }
+
+        public string GetAccountHistory()
+        {
+            var report = new System.Text.StringBuilder();
+
+            decimal balance = 0;
+            report.AppendLine("Date\t\tAmount\tBalance\tNote");
+            foreach (var item in allTransactions)
+            {
+                balance += item.Amount;
+                report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{balance}\t{item.Notes}");
+            }
+
+            return report.ToString();
+        }
     }
 }
--- C# codes/C#101/OOP/OOP1/Program.cs
+++ C# codes/C#101/OOP/OOP1/Program.cs
@@ -9,6 +9,9 @@ namespace MySuperBank
         {
             BankAccount account = new BankAccount("Test",400);
             Console.WriteLine($"Account {account.Number} is created for {account.Owner} with {account.Balance} dollar(s).");
+            account.MakeDeposit(100, DateTime.Now, "Deposit");
+            account.MakeWithdrawal(355, DateTime.Now, "Withdraw");
+            Console.WriteLine(account.GetAccountHistory());
             // Test for a negative balance.
             try
             {
