7f663506169729d232d76a68f68bf8ca6b6bba00
GeorgeDong32 <georgedong32@foxmail.com>
2022-08-05 15:30:06 +0800

    Init

---
+++ C# codes/C#101/OOP/OOP1/BankAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MySuperBank
+{
+    public class BankAccount
+    {
+        public string Number { get; set; }
+        public string Owner { get; set; }
+        public decimal Balance { get; set; }
+        private static int accountNumberSeed = 11111111;
+        public BankAccount(string owner, decimal balance)
+        {
+            Number = accountNumberSeed.ToString();
+            Owner = owner;
+            Balance = balance;
+            accountNumberSeed++;
+        }
+    
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+
+        }
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+
+        }
+    }
+}
--- C# codes/C#101/OOP/OOP1/Program.cs
+++ C# codes/C#101/OOP/OOP1/Program.cs
@@ -10,6 +10,8 @@ namespace MySuperBank
     {
         static void Main(string[] args)
         {
+            BankAccount account = new BankAccount("Test", 10000);
+            Console.WriteLine($"Account {account.Number} is created for {account.Owner} with {account.Balance} dollar(s).");
         }
     }
 }
