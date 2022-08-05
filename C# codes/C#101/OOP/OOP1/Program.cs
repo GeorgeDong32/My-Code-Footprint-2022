bdf8dd5dad582b1f8d14bf5584f1a508a07842b4
GeorgeDong32 <georgedong32@foxmail.com>
2022-08-05 16:39:27 +0800

    Update

--- C# codes/C#101/OOP/OOP1/Program.cs
+++ C# codes/C#101/OOP/OOP1/Program.cs
@@ -9,10 +9,16 @@ namespace MySuperBank
         {
             BankAccount account = new BankAccount("Test",400);
             Console.WriteLine($"Account {account.Number} is created for {account.Owner} with {account.Balance} dollar(s).");
-            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
-            Console.WriteLine(account.Balance);
-            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
-            Console.WriteLine(account.Balance);
+            // Test for a negative balance.
+            try
+            {
+                account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Exception caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
