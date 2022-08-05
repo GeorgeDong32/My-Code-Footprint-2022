1fbfccead20adf758ed7d3839e7801e61c1603b2
GeorgeDong32 <georgedong32@foxmail.com>
2022-08-05 16:09:10 +0800

    Up

--- C# codes/C#101/OOP/OOP1/BankAccount.cs
+++ C# codes/C#101/OOP/OOP1/BankAccount.cs
@@ -7,25 +7,52 @@ namespace Classes
     {
         public string Number { get; set; }
         public string Owner { get; set; }
-        public decimal Balance { get; set; }
+        public decimal Balance { 
+            get
+            { 
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+                return Balance;
+            }
+            set
+            { 
+            } 
+        }
         private List<Transaction> allTransactions = new List<Transaction>();
         private static int accountNumberSeed = 11111111;
-        public BankAccount(string owner, decimal balance)
+        public BankAccount(string owner)
         {
             Number = accountNumberSeed.ToString();
             Owner = owner;
-            Balance = balance;
             accountNumberSeed++;
 
         }
-    
+
         public void MakeDeposit(decimal amount, DateTime date, string note)
         {
-
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
         }
+
         public void MakeWithdrawal(decimal amount, DateTime date, string note)
         {
-
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
         }
     }
 }
