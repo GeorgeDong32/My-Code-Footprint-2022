a17700ff458449982f3ec541390d4cba66fb26af
GeorgeDong32 <georgedong32@foxmail.com>
2022-08-06 23:14:44 +0800

    Update

--- C# codes/Docs Learn/Data Type Learn/Program.cs
+++ C# codes/Docs Learn/Data Type Learn/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿(double, int) t1 = (4.5, 3);
+Console.WriteLine($"Tuple with elements {t1.Item1} and {t1.Item2}.");
+// Output:
+// Tuple with elements 4.5 and 3.
+
+(double Sum, int Count) t2 = (4.5, 3);
+Console.WriteLine($"Sum of {t2.Count} elements is {t2.Sum}.");
+// Output:
+// Sum of 3 elements is 4.5.
+
+(double, int) t = (4.5, 3);
+Console.WriteLine(t.ToString());
+Console.WriteLine($"Hash code of {t} is {t.GetHashCode()}.");
+// Output:
+// (4.5, 3)
+// Hash code of (4.5, 3) is 718460086.
+
+var xs = new[] { 4, 7, 9 };
+var limits = FindMinMax(xs);
+Console.WriteLine($"Limits of [{string.Join(" ", xs)}] are {limits.min} and {limits.max}");
+// Output:
+// Limits of [4 7 9] are 4 and 9
+
+var ys = new[] { -9, 0, 67, 100 };
+var (minimum, maximum) = FindMinMax(ys);
+Console.WriteLine($"Limits of [{string.Join(" ", ys)}] are {minimum} and {maximum}");
+// Output:
+// Limits of [-9 0 67 100] are -9 and 100
+
+(int min, int max) FindMinMax(int[] input)
+{
+    if (input is null || input.Length == 0)
+    {
+        throw new ArgumentException("Cannot find minimum and maximum of a null or empty array.");
+    }
+
+    var min = int.MaxValue;
+    var max = int.MinValue;
+    foreach (var i in input)
+    {
+        if (i < min)
+        {
+            min = i;
+        }
+        if (i > max)
+        {
+            max = i;
+        }
+    }
+    return (min, max);
+}
