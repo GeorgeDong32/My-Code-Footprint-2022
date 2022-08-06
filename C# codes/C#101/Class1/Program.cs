262ef8298a691257d4cd3ccfe7d8e9b41566638c
GeorgeDong32 <georgedong32@foxmail.com>
2022-08-06 20:22:36 +0800

    Init

---
+++ C# codes/C#101/Class1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Class1
+{
+    public enum SomeRootVegetable
+    {
+        HorseRadish,
+        Radish,
+        Turnip
+    }
+    public enum Seasons
+    {
+        None = 0,
+        Summer = 1,
+        Autumn = 2,
+        Winter = 4,
+        Spring = 8,
+        All = Summer | Autumn | Winter | Spring
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var turnip = SomeRootVegetable.Turnip;
+            var spring = Seasons.Spring;
+            var startingOnEquinox = Seasons.Spring | Seasons.Autumn;
+            var theYear = Seasons.All;
+        }
+    }
+}
