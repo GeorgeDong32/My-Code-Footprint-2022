f6983477c031df2429a5e2728ed2e5643b07f073
GeorgeDong32 <georgedong32@foxmail.com>
2022-08-07 23:37:44 +0800

    Init

--- C# codes/Docs Learn/Build Block/Program.cs
+++ C# codes/Docs Learn/Build Block/Program.cs
@@ -247,3 +247,33 @@ class EventExample
         Console.WriteLine(s_changeCount); // "3"
     }
 }*/
+//委托
+delegate double Function(double x);
+
+class Multiplier
+{
+    double _factor;
+
+    public Multiplier(double factor) => _factor = factor;
+
+    public double Multiply(double x) => x * _factor;
+}
+
+class DelegateExample
+{
+    static double[] Apply(double[] a, Function f)
+    {
+        var result = new double[a.Length];
+        for (int i = 0; i < a.Length; i++) result[i] = f(a[i]);
+        return result;
+    }
+
+    public static void Main()
+    {
+        double[] a = { 0.0, 0.5, 1.0 };
+        double[] squares = Apply(a, (x) => x * x);
+        double[] sines = Apply(a, Math.Sin);
+        Multiplier m = new(2.0);
+        double[] doubles = Apply(a, m.Multiply);
+    }
+}
