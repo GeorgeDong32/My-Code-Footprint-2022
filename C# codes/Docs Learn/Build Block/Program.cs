9a93d566993f05c1898d672a35d26037e74a2efd
GeorgeDong32 <georgedong32@foxmail.com>
2022-08-07 21:53:37 +0800

    Update

--- C# codes/Docs Learn/Build Block/Program.cs
+++ C# codes/Docs Learn/Build Block/Program.cs
@@ -31,7 +31,7 @@ class Entity
     }
 }*/
 //虚方法、抽象方法和方法重写
-Expression e = new Operation(
+/*Expression e = new Operation(
     new VariableReference("x"),
     '*',
     new Operation(
@@ -110,5 +110,28 @@ public class Operation : Expression
             default: throw new Exception("Unknown operator");
         }
     }
-}
+}*/
+//方法重载
+var o = new OverloadingExample();
+OverloadingExample.UsageExample();
+class OverloadingExample
+{
+    static void F() => Console.WriteLine("F()");
+    static void F(object x) => Console.WriteLine("F(object)");
+    static void F(int x) => Console.WriteLine("F(int)");
+    static void F(double x) => Console.WriteLine("F(double)");
+    static void F<T>(T x) => Console.WriteLine($"F<T>(T), T is {typeof(T)}");
+    static void F(double x, double y) => Console.WriteLine("F(double, double)");
 
+    public static void UsageExample()
+    {
+        F();            // Invokes F()
+        F(1);           // Invokes F(int)
+        F(1.0);         // Invokes F(double)
+        F("abc");       // Invokes F<T>(T), T is System.String
+        F((double)1);   // Invokes F(double)
+        F((object)1);   // Invokes F(object)
+        F<int>(1);      // Invokes F<T>(T), T is System.Int32
+        F(1, 1);        // Invokes F(double, double)
+    }
+}
