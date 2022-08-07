1ea3c70c106712bf6505cb06a51b745fb3aaf554
GeorgeDong32 <georgedong32@foxmail.com>
2022-08-07 20:32:57 +0800

    Update

--- C# codes/Docs Learn/Build Block/Program.cs
+++ C# codes/Docs Learn/Build Block/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Entity.SetNextSerialNo(1000);
+Entity e1 = new();
+Entity e2 = new();
+Console.WriteLine(e1.GetSerialNo());          // Outputs "1000"
+Console.WriteLine(e2.GetSerialNo());          // Outputs "1001"
+Console.WriteLine(Entity.GetNextSerialNo());  // Outputs "1002"
+class Entity
+{
+    static int s_nextSerialNo;
+    int _serialNo;
+
+    public Entity()
+    {
+        _serialNo = s_nextSerialNo++;
+    }
+
+    public int GetSerialNo()
+    {
+        return _serialNo;
+    }
+
+    public static int GetNextSerialNo()
+    {
+        return s_nextSerialNo;
+    }
+
+    public static void SetNextSerialNo(int value)
+    {
+        s_nextSerialNo = value;
+    }
+}
