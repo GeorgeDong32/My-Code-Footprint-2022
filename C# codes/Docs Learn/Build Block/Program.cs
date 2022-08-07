ef2d892416f2aa94c25fa4838e7105fdec81a759
GeorgeDong32 <georgedong32@foxmail.com>
2022-08-07 23:37:04 +0800

    Init

--- C# codes/Docs Learn/Build Block/Program.cs
+++ C# codes/Docs Learn/Build Block/Program.cs
@@ -135,3 +135,115 @@ class OverloadingExample
         F(1, 1);        // Invokes F(double, double)
     }
 }*/
+//其他函数成员
+/*MyList<string> list1 = new();
+MyList<string> list2 = new(10);
+MyList<string> names = new();
+names.Capacity = 100;   // Invokes set accessor
+//int i = names.Count;    // Invokes get accessor
+int j = names.Capacity; // Invokes get accessor
+names.Add("Liz");
+names.Add("Martha");
+names.Add("Beth");
+for (int i = 0; i < names.Count; i++)
+{
+    string s = names[i];
+    names[i] = s.ToUpper();
+}
+EventExample.Usage();
+
+public class MyList<T>
+{
+    const int DefaultCapacity = 4;
+
+    T[] _items;
+    int _count;
+
+    public MyList(int capacity = DefaultCapacity)
+    {
+        _items = new T[capacity];
+    }
+
+    public int Count => _count;
+
+    public int Capacity
+    {
+        get => _items.Length;
+        set
+        {
+            if (value < _count) value = _count;
+            if (value != _items.Length)
+            {
+                T[] newItems = new T[value];
+                Array.Copy(_items, 0, newItems, 0, _count);
+                _items = newItems;
+            }
+        }
+    }
+
+    public T this[int index]
+    {
+        get => _items[index];
+        set
+        {
+            _items[index] = value;
+            OnChanged();
+        }
+    }
+
+    public void Add(T item)
+    {
+        if (_count == Capacity) Capacity = _count * 2;
+        _items[_count] = item;
+        _count++;
+        OnChanged();
+    }
+    protected virtual void OnChanged() =>
+        Changed?.Invoke(this, EventArgs.Empty);
+
+    public override bool Equals(object other) =>
+        Equals(this, other as MyList<T>);
+
+    static bool Equals(MyList<T> a, MyList<T> b)
+    {
+        if (Object.ReferenceEquals(a, null)) return Object.ReferenceEquals(b, null);
+        if (Object.ReferenceEquals(b, null) || a._count != b._count)
+            return false;
+        for (int i = 0; i < a._count; i++)
+        {
+            if (!object.Equals(a._items[i], b._items[i]))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public event EventHandler Changed;
+
+    public static bool operator ==(MyList<T> a, MyList<T> b) =>
+        Equals(a, b);
+
+    public static bool operator !=(MyList<T> a, MyList<T> b) =>
+        !Equals(a, b);
+}
+
+class EventExample
+{
+    static int s_changeCount;
+
+    static void ListChanged(object sender, EventArgs e)
+    {
+        s_changeCount++;
+    }
+
+    public static void Usage()
+    {
+        var names = new MyList<string>();
+        names.Changed += new EventHandler(ListChanged);
+        names.Add("Liz");
+        names.Add("Martha");
+        names.Add("Beth");
+        Console.WriteLine(s_changeCount); // "3"
+    }
+}*/
