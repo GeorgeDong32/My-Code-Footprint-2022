5da2acc645e1afd63b764a3e1aa7fa880750b497
GeorgeDong32 <georgedong32@foxmail.com>
2022-08-08 17:55:07 +0800

    Update

--- C# codes/Docs Learn/delegate&lambda/Program.cs
+++ C# codes/Docs Learn/delegate&lambda/Program.cs
@@ -7,3 +7,11 @@ System.Linq.Expressions.Expression<Func<int, int>> e = x => x * x;
 Console.WriteLine(e);
 // Output:
 // x => (x * x)
+//空括号表示没有入参
+Action line = () => Console.WriteLine();
+//只有一个参数可以省略括号
+Func<double, double> cube = (x) => x * x * x;
+Console.WriteLine(cube(3));
+
+Func<int, int, int> constant = (_, _) => 42;
+Console.WriteLine(constant(0, 0));
