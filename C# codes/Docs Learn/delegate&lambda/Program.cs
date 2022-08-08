5f3549035108a70af242d93228305cf16c6e911a
GeorgeDong32 <georgedong32@foxmail.com>
2022-08-08 15:41:53 +0800

    Update

--- C# codes/Docs Learn/delegate&lambda/Program.cs
+++ C# codes/Docs Learn/delegate&lambda/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Func<int, int> square = x => x * x;
+Console.WriteLine(square(5));
+// Output:
+// 25
+
+System.Linq.Expressions.Expression<Func<int, int>> e = x => x * x;
+Console.WriteLine(e);
+// Output:
+// x => (x * x)
