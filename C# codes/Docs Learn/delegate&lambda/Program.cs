ddbe7264e4057cef3b8b145c01f2847892f5be1a
GeorgeDong32 <georgedong32@foxmail.com>
2022-08-08 20:47:18 +0800

    Update

--- C# codes/Docs Learn/delegate&lambda/Program.cs
+++ C# codes/Docs Learn/delegate&lambda/Program.cs
@@ -36,14 +36,26 @@ Console.WriteLine(constant(0, 0));*/
     }
 }*/
 //First delegate
-Logger.WriteMessage += LoggingMethods.LogToConsole;
+var file = new FileLogger("log.txt");
+var fileOutput = new FileLogger("log.txt");
+Logger.WriteMessage += LoggingMethods.LogToConsole; // LoggingMethods is the static class we utilized earlier
+public enum Severity
+{
+    Verbose,
+    Trace,
+    Information,
+    Warning,
+    Error,
+    Critical
+}
 public static class Logger
 {
     public static Action<string> WriteMessage;
 
+    public static Severity LogLevel { get; set; } = Severity.Warning;
     public static void LogMessage(string msg)
     {
-        WriteMessage(msg);
+        WriteMessage?.Invoke(msg);
     }
 }
 public static class LoggingMethods
@@ -51,5 +63,37 @@ public static class LoggingMethods
     public static void LogToConsole(string message)
     {
         Console.Error.WriteLine(message);
+    }
+}
+public class FileLogger
+{
+    private readonly string logPath;
+    public FileLogger(string path)
+    {
+        logPath = path;
+        Logger.WriteMessage += LogMessage;
+    }
+
+    public void DetachLog() => Logger.WriteMessage -= LogMessage;
+    // make sure this can't throw.
+    private void LogMessage(string msg)
+    {
+        try
+        {
+            using (var log = File.AppendText(logPath))
+            {
+                log.WriteLine(msg);
+                log.Flush();
+            }
+        }
+        catch (Exception)
+        {
+            // Hmm. We caught an exception while
+            // logging. We can't really log the
+            // problem (since it's the log that's failing).
+            // So, while normally, catching an exception
+            // and doing nothing isn't wise, it's really the
+            // only reasonable option here.
+        }
     }
 }
