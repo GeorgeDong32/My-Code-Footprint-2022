f8d44f857ca0e0061bfb57e1846e09e5987cae0f
GeorgeDong32 <georgedong32@foxmail.com>
2022-08-09 21:56:39 +0800

    Update

--- C# codes/Docs Learn/类型系统/Program.cs
+++ C# codes/Docs Learn/类型系统/Program.cs
@@ -110,3 +110,8 @@ var v = new { Amount = 108, Message = "Hello" };
 // Rest the mouse pointer over v.Amount and v.Message in the following
 // statement to verify that their inferred types are int and string.
 Console.WriteLine(v.Amount + v.Message);
+var anonArray = new[] { new { name = "apple", diam = 4 }, new { name = "grape", diam = 1 } };
+var apple = new { Item = "apples", Price = 1.35 };
+var onSale = apple with { Price = 0.79 };
+Console.WriteLine(apple);
+Console.WriteLine(onSale);
