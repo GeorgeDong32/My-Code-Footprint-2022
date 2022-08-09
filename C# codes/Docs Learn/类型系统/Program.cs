add12c78e8f8ffca25f71a47a0570d15e138d3bf
GeorgeDong32 <georgedong32@foxmail.com>
2022-08-09 13:43:25 +0800

    Update

--- C# codes/Docs Learn/类型系统/Program.cs
+++ C# codes/Docs Learn/类型系统/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Interface
+Car car1 = new Car();
+Car car2 = new Car();
+car1.Make = "German";
+car1.Model = "BMW";
+car1.Year = "2022";
+car2.Make = "Italy";
+car2.Model = "Ferrari";
+car2.Year = "1998";
+Console.WriteLine(car1 == car2);
+interface IEquatable<T>
+{
+    bool Equals(T obj);
+}
+public class Car : IEquatable<Car>
+{
+    public string? Make { get; set; }
+    public string? Model { get; set; }
+    public string? Year { get; set; }
+
+    // Implementation of IEquatable<T> interface
+    public bool Equals(Car? car)
+    {
+        return (this.Make, this.Model, this.Year) ==
+            (car?.Make, car?.Model, car?.Year);
+    }
+}
