544acfec01461379f3c62a9e2b37519355b2338a
GeorgeDong32 <georgedong32@foxmail.com>
2022-08-09 21:12:37 +0800

    Update

--- C# codes/Docs Learn/类型系统/Program.cs
+++ C# codes/Docs Learn/类型系统/Program.cs
@@ -27,25 +27,61 @@ public class Car : IEquatable<Car>
 }*/
 //泛型
 // Declare the generic class.
+// type parameter T in angle brackets
 public class GenericList<T>
 {
-    public void Add(T input) { }
-}
-class TestGenericList
-{
-    private class ExampleClass { }
-    static void Main()
+    // The nested class is also generic on T.
+    private class Node
     {
-        // Declare a list of type int.
-        GenericList<int> list1 = new GenericList<int>();
-        list1.Add(1);
+        // T used in non-generic constructor.
+        public Node(T t)
+        {
+            next = null;
+            data = t;
+        }
 
-        // Declare a list of type string.
-        GenericList<string> list2 = new GenericList<string>();
-        list2.Add("");
+        private Node? next;
+        public Node? Next
+        {
+            get { return next; }
+            set { next = value; }
+        }
 
-        // Declare a list of type ExampleClass.
-        GenericList<ExampleClass> list3 = new GenericList<ExampleClass>();
-        list3.Add(new ExampleClass());
+        // T as private member data type.
+        private T data;
+
+        // T as return type of property.
+        public T Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+    }
+
+    private Node? head;
+
+    // constructor
+    public GenericList()
+    {
+        head = null;
+    }
+
+    // T as method parameter type:
+    public void AddHead(T t)
+    {
+        Node n = new Node(t);
+        n.Next = head;
+        head = n;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        Node? current = head;
+
+        while (current != null)
+        {
+            yield return current.Data;
+            current = current.Next;
+        }
     }
 }
