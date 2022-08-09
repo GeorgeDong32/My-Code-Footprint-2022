cd76f9a31c8740e813dd6ead45cc9f5bdeda5cdf
GeorgeDong32 <georgedong32@foxmail.com>
2022-08-09 21:56:59 +0800

    Init

--- C# codes/Docs Learn/类型系统/Program.cs
+++ C# codes/Docs Learn/类型系统/Program.cs
@@ -105,7 +105,7 @@ class TestGenericList
         System.Console.WriteLine("\nDone");
     }
 }*/
-var v = new { Amount = 108, Message = "Hello" };
+/*var v = new { Amount = 108, Message = "Hello" };
 
 // Rest the mouse pointer over v.Amount and v.Message in the following
 // statement to verify that their inferred types are int and string.
@@ -114,4 +114,37 @@ var anonArray = new[] { new { name = "apple", diam = 4 }, new { name = "grape", 
 var apple = new { Item = "apples", Price = 1.35 };
 var onSale = apple with { Price = 0.79 };
 Console.WriteLine(apple);
-Console.WriteLine(onSale);
+Console.WriteLine(onSale);*/
+public partial class Coords
+{
+    private int x;
+    private int y;
+
+    public Coords(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+public partial class Coords
+{
+    public void PrintCoords()
+    {
+        Console.WriteLine("Coords: {0},{1}", x, y);
+    }
+}
+
+class TestCoords
+{
+    static void Main()
+    {
+        Coords myCoords = new Coords(10, 15);
+        myCoords.PrintCoords();
+
+        // Keep the console window open in debug mode.
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+    }
+}
+// Output: Coords: 10,15
