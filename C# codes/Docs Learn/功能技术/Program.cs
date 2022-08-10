1241090a1ecf871a18190f68f54905d843e2c879
GeorgeDong32 <georgedong32@foxmail.com>
2022-08-10 21:20:14 +0800

    Update

--- C# codes/Docs Learn/功能技术/Program.cs
+++ C# codes/Docs Learn/功能技术/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int? maybe = 12;
+
+if (maybe is int number)
+{
+    Console.WriteLine($"The nullable int 'maybe' has the value {number}");
+}
+else
+{
+    Console.WriteLine("The nullable int 'maybe' doesn't hold a value");
+}
