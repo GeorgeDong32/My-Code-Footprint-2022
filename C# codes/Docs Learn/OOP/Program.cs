d11d628bbe9e06ee258144017f5b163a963e7ff6
GeorgeDong32 <georgedong32@foxmail.com>
2022-08-10 18:04:14 +0800

    Update

--- C# codes/Docs Learn/OOP/Program.cs
+++ C# codes/Docs Learn/OOP/Program.cs
@@ -183,8 +183,28 @@ public class Triangle : Shape
         base.Draw();
     }
 }*/
-//重写
+/*重写*/
+DerivedClass B = new DerivedClass();
+B.DoWork();  // Calls the new method.
 
+BaseClass A = B;
+A.DoWork();  // Also calls the new method.
+public class BaseClass
+{
+    public virtual void DoWork() { }
+    public virtual int WorkProperty
+    {
+        get { return 0; }
+    }
+}
+public class DerivedClass : BaseClass
+{
+    public override void DoWork() { }
+    public override int WorkProperty
+    {
+        get { return 0; }
+    }
+}
 
 //覆盖
 
