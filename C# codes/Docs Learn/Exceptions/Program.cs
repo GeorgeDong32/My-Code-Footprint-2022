bdece5e7c7f00404ec5d35b3fe35c9c89244a69a
GeorgeDong32 <georgedong32@foxmail.com>
2022-08-12 16:15:50 +0800

    Update

--- C# codes/Docs Learn/Exceptions/Program.cs
+++ C# codes/Docs Learn/Exceptions/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Short*/
+public class ExceptionTest
+{
+    static double SafeDivision(double x, double y)
+    {
+        if (y == 0)
+            throw new DivideByZeroException();
+        return x / y;
+    }
+
+    public static void Main()
+    {
+        // Input for test purposes. Change the values to see
+        // exception handling behavior.
+        double a = 98, b = 0;
+        double result;
+
+        try
+        {
+            result = SafeDivision(a, b);
+            Console.WriteLine("{0} divided by {1} = {2}", a, b, result);
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Attempted divide by zero.");
+        }
+    }
+}
