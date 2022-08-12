cd9f0bfc47d62daa4fb1fc603da99eddf8b2f01a
GeorgeDong32 <georgedong32@foxmail.com>
2022-08-12 20:13:05 +0800

    Init

--- C# codes/Docs Learn/Exceptions/Program.cs
+++ C# codes/Docs Learn/Exceptions/Program.cs
@@ -26,7 +26,7 @@ public class ExceptionTest
         }
     }
 }*/
-/*使用异常*/
+/*使用异常
 try
 {
     TestThrow();
@@ -44,5 +44,41 @@ class CustomException : Exception
     public CustomException(string message)
     {
     }
-}
+}*/
+/*使用异常2
+using System;
+using System.IO;
 
+namespace Exceptions
+{
+    public class CatchOrder
+    {
+        public static void Main()
+        {
+            try
+            {
+                using (var sw = new StreamWriter("./test.txt"))
+                {
+                    sw.WriteLine("Hello");
+                }
+            }
+            // Put the more specific exceptions first.
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            // Put the least specific exception last.
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            Console.WriteLine("Done");
+        }
+    }
+}
+*/
+/*TestFinally*/
