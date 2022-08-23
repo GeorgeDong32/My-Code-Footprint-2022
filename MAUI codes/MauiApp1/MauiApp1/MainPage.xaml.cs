440b69c8a6ab7c9f47faac1b693760bf89424c9d
GeorgeDong32 <georgedong32@foxmail.com>
2022-08-23 23:53:50 +0800

    Try

--- MAUI codes/MauiApp1/MauiApp1/MainPage.xaml.cs
+++ MAUI codes/MauiApp1/MauiApp1/MainPage.xaml.cs
@@ -14,11 +14,19 @@ public partial class MainPage : ContentPage
 		count++;
 
 		if (count == 1)
+		{
 			CounterBtn.Text = $"Clicked {count} time";
+			MyCounter.Text = $"Clicked {count} time";
+        }
+
 		else
-			CounterBtn.Text = $"Clicked {count} times";
+		{ 
+			CounterBtn.Text = $"Clicked {count} times"; 
+			MyCounter.Text = $"Clicked {count} times";
+        }
 
 		SemanticScreenReader.Announce(CounterBtn.Text);
+		SemanticScreenReader.Announce(MyCounter.Text);
 	}
 }
 
