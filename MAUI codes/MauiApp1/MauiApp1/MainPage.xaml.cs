50eb086232f1f171ffcb65dbbf374d3f268d0611
GeorgeDong32 <georgedong32@foxmail.com>
2022-08-25 18:30:53 +0800

    Test Button

--- MAUI codes/MauiApp1/MauiApp1/MainPage.xaml.cs
+++ MAUI codes/MauiApp1/MauiApp1/MainPage.xaml.cs
@@ -16,17 +16,27 @@ public partial class MainPage : ContentPage
 		if (count == 1)
 		{
 			CounterBtn.Text = $"Clicked {count} time";
-			MyCounter.Text = $"Clicked {count} time";
+			//MyCounter.Text = $"Clicked {count} time";
         }
 
 		else
 		{ 
 			CounterBtn.Text = $"Clicked {count} times"; 
-			MyCounter.Text = $"Clicked {count} times";
+			//MyCounter.Text = $"Clicked {count} times";
         }
 
 		SemanticScreenReader.Announce(CounterBtn.Text);
-		SemanticScreenReader.Announce(MyCounter.Text);
+		//SemanticScreenReader.Announce(MyCounter.Text);
 	}
+
+    private void MyBtn_Clicked(object sender, EventArgs e)
+	{
+		var rand = new Random();
+		var colorint = rand.Next(0, 255);
+		var color = Color.FromRgba(rand.Next(), rand.Next(0, 255), rand.Next(0, 255), 120);
+		Console.WriteLine($"The int of color is{colorint}");
+		MyBtn.TextColor = color;
+        
+    }
 }
 
