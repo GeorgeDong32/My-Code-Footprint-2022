76ef8e490c1da5e9ebadd6a6a209e3304e016407
GeorgeDong32 <georgedong32@foxmail.com>
2022-09-08 16:33:41 +0800

    Chang File DIr(WBS)

--- MAUI codes/exercise1/Notes/MainPage.xaml.cs
+++ MAUI codes/exercise1/Notes/MainPage.xaml.cs
@@ -2,7 +2,7 @@
 
 public partial class MainPage : ContentPage
 {
-    string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
+    string _fileName = Path.Combine("D:\\My-Code-Footprint\\MAUI codes\\exercise1\\Notes", "notes.txt");
     Editor editor;
 
     public MainPage()
