f65e8fa213cd8b4cd81bb38032b9ef64a90e54d8
GeorgeDong32 <georgedong32@foxmail.com>
2022-09-27 13:04:59 +0800

    Update

---
+++ MAUI codes/store-local-data/People/People/PersonRepository.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using People.Models;
+
+namespace People
+{
+    public class PersonRepository
+    {
+        string _dbPath;
+
+        private SQLiteConnection conn;
+
+        public string StatusMessage { get; set; }
+
+        private void Init()
+        {
+            if (conn != null)
+                return;
+
+            conn = new SQLiteConnection(_dbPath);
+            conn.CreateTable<Person>();
+        }
+
+        public PersonRepository(string dbPath)
+        {
+            _dbPath = dbPath;                        
+        }
+
+        public void AddNewPerson(string name)
+        {            
+            int result = 0;
+            try
+            {
+                Init();
+
+                // basic validation to ensure a name was entered
+                if (string.IsNullOrEmpty(name))
+                    throw new Exception("Valid name required");
+
+                result = conn.Insert(new Person { Name = name });
+
+                StatusMessage = string.Format("{0} record(s) added (Name: {1})", result, name);
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = string.Format("Failed to add {0}. Error: {1}", name, ex.Message);
+            }
+
+        }
+
+        public List<Person> GetAllPeople()
+        {
+            try
+            {
+                Init();
+                return conn.Table<Person>().ToList();
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = string.Format("Failed to retrieve data. {0}", ex.Message);
+            }
+
+            return new List<Person>();
+        }
+    }
+}
